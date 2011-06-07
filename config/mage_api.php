9bdbaf53ad306385b2b9abfe280213d8bc8579b7
Centerax <pablos.benitez@gmail.com>
2011-06-07 00:16:48 -0300

    config file template

--- config/mage_api.php
+++ config/mage_api.php
@@ -1,5 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config ['magento_wsdl_uri']     = 'http://centerax.no-ip.org/api/soap?wsdl';
-$config ['magento_api_username'] = 'pablob';
-$config ['magento_api_password'] = 'shakalala';
+$config ['magento_wsdl_uri']     = '';
+$config ['magento_api_username'] = '';
+$config ['magento_api_password'] = '';
